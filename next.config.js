bf5ae1a705d9c7023cf140a5a0531df57cc889ab
ArturBortniczuk <a.bortniczuk@grupaeltron.pl>
2025-03-25 12:56:47 +0100

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -2,11 +2,13 @@
 const nextConfig = {
   webpack: (config) => {
     // Wykluczamy niepotrzebne dialekty
-    config.externals = [...(config.externals || []), 'better-sqlite3', 'oracledb', 'pg-query-stream'];
+    config.externals = [...(config.externals || []), 'better-sqlite3', 'sqlite3', 'oracledb', 'pg-query-stream'];
     
     // Dodajemy aliasy dla nieużywanych dialektów
     config.resolve.alias = {
       ...config.resolve.alias,
+      'better-sqlite3': false,
+      'sqlite3': false,
       'oracledb': false,
       'pg-query-stream': false,
     };
