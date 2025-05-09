66ee2bd205d3da1348dd87c4afa3518047e194b3
ArturBortniczuk <a.bortniczuk@grupaeltron.pl>
2025-05-09 12:21:30 +0200

    Update TransportForm.js

--- src/app/kalendarz/components/TransportForm.js
+++ src/app/kalendarz/components/TransportForm.js
@@ -5,6 +5,7 @@ import { pl } from 'date-fns/locale'
 import LocationSelector from './LocationSelector'
 import ConstructionSelector from './ConstructionSelector'
 import { ChevronRight } from 'lucide-react' // Ikona dla połączonych tras
+import { wyslijPowiadomienieOdbioruBebnow } from '../utils/smsNotifications';
 
 export default function TransportForm({
   selectedDate,
